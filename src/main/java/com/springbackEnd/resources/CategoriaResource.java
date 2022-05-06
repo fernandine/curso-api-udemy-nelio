7a46776c65c8a9dd630bd7a3e490ee444781c467
Jean Fernandine <jfernandine@gmail.com>
2022-05-06 10:22:49 -0300

    categoria DTO

--- src/main/java/com/springbackEnd/resources/CategoriaResource.java
+++ src/main/java/com/springbackEnd/resources/CategoriaResource.java
@@ -1,10 +1,13 @@
 package com.springbackEnd.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -14,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.springbackEnd.DTOs.CategoriaDto;
 import com.springbackEnd.domain.Categoria;
 import com.springbackEnd.services.CategoriaService;
 
@@ -49,5 +53,11 @@ public class CategoriaResource {
 	public ResponseEntity<Void> delete(@PathVariable Integer id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+	@GetMapping
+	public ResponseEntity<List<CategoriaDto>> findAll() {
+		List<Categoria> list = service.findAll();
+		List<CategoriaDto> listDto = list.stream().map(obj -> new CategoriaDto(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 }
